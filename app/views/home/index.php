8e25f948707a14bf21e18ab566f33e8933f221f8
xopxop <xopxop.1403@gmail.com>
2021-05-24 06:51:50 +0300

    added webcam

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,13 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-	<h1><?php echo $data['title']?></h1>
+<div class="row">
+  <main class="col-sm-9 card" style="min-height: 850px">
+    <h1><?php echo $data['title']?></h1>
+    <div>
+      <video autoplay="true" id="webcam"></video>
+    </div>
+  </main>
+  <side class="col-sm-3 card">
+    <h1>side</h1>
+  </side>
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -3,5 +3,8 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
   <script src="<?php echo URLROOT; ?>/js/main.js"></script>
+  <footer class="text-center text-white fixed-bottom" style="background-color: black;">
+  	<div class="text-center p-3">&copy; Copy right:</div>
+  </footer>
 </body>
 </html>
