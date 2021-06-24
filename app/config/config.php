9d0acad2d6ed1af96170794568df219b9ff4daef
xopxop <xopxop.1403@gmail.com>
2021-06-24 23:23:06 +0300

    Entering point set-up

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
-	//DB Params
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'root');
-	define('DB_NAME', 'Camagru');
+  //DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'Camagru');
 
-	//App Root
-	define('APPROOT', dirname(dirname(__FILE__)));
+  //App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
 
-	//URL Root
-	define('URLROOT', 'http://localhost/Camagru');
+  //URL Root
+  define('URLROOT', 'http://localhost/Camagru');
 
-	//Site name
-	define('SITENAME', 'Camagru');
+  //Site name
+  define('SITENAME', 'Camagru');
