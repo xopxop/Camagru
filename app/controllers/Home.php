235ce587ffeb2e921f7bc5b75f82a6cf6ebb922c
dthan <46009952+xopxop@users.noreply.github.com>
2021-06-24 23:24:07 +0300

    Entering point set-up (#3)
    MVC setup: set entering point

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,21 +1,21 @@
 <?php
-	class Home extends Controller {
-		public function __construct() {
-		}
+  class Home extends Controller {
+    public function __construct() {
+    }
 
-		public function index() {
-			$data = [
-				'title' => 'Camagru',
-			];
+    public function index() {
+      $data = [
+        'title' => 'Camagru',
+      ];
 
-			$this->view('home/index', $data);
-		}
+      $this->view('home/index', $data);
+    }
 
-		public function about() {
-			$data = [
-				'title' => 'About Us'
-			];
+    public function about() {
+      $data = [
+        'title' => 'About Us'
+      ];
 
-			$this->view('home/about', $data);
-		}
-	}
+      $this->view('home/about', $data);
+    }
+  }
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -8,21 +8,21 @@
       $url = $this->getUrl();
 
       if (isset($url[0])) {
-	if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-	  $this->currentController = ucwords($url[0]);
-	  unset($url[0]);
-	}
+	      if (file_exists('./app/controllers/' . ucwords($url[0]) . '.php')) {
+	        $this->currentController = ucwords($url[0]);
+	        unset($url[0]);
+	      }
       }
 
-      require_once '../app/controllers/' . $this->currentController . '.php';
+      require_once './app/controllers/' . $this->currentController . '.php';
 
       $this->currentController = new $this->currentController;
 
       if (isset($url[1])) {
-	if (method_exists($this->currentController, $url[1])) {
-	  $this->currentMethod = $url[1];
-	  unset($url[1]);
-	}
+	      if (method_exists($this->currentController, $url[1])) {
+	        $this->currentMethod = $url[1];
+	        unset($url[1]);
+	      }
       }
 
       $this->params = $url ? array_values($url) : [];
@@ -31,10 +31,10 @@
 
     public function getUrl() {
       if (isset($_GET['url'])) {
-	$url = rtrim($_GET['url'], '/');
-	$url = filter_var($url, FILTER_SANITIZE_URL);
-	$url = explode('/', $url);
-	return $url;
+	      $url = rtrim($_GET['url'], '/');
+	      $url = filter_var($url, FILTER_SANITIZE_URL);
+	      $url = explode('/', $url);
+	      return $url;
       }
     }
   }
