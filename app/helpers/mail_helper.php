d4b008465e8cc9dd890c8ce9e21c2ab708e2746d
du than <du.than@relexsolutions.com>
2023-11-12 14:58:41 +0200

    refresh

--- app/helpers/mail_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Send mail
-  function sendMail($data) {
-    $subject = "[Camagru]Confirm E-mail Address";
-    $message = "Welcome " . $data['name'] . "!\nThanks for signing up with Camagru!";
-
-    mail($data['email'], $subject, $message);
-  }
